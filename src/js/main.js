7d335c92f56bfc2641b1c084fca83d5749237ac5
mazax1ct <vova@giveandget.ru>
2019-09-24 15:53:55 +0300

    header refactor + news + typo

--- src/js/main.js
+++ src/js/main.js
@@ -34,35 +34,35 @@ $(document).ready(function () {
   //открваем мобильное меню
   $('.js-menu-open').click(function () {
     $(this).toggleClass('is-active');
-    $('.header').toggleClass('m-menu-open');
+    $('.header').toggleClass('header--menu_open');
     $('.body').toggleClass('overflow');
-    $('.mobile-menu').toggleClass('is-open');
+    $('.menu-block').toggleClass('is-open');
     return false;
   });
 
   //мобильное выпадающее меню 2 уровня
-  $('.js-m-root-1').click(function () {
+  $('.js-root-1').click(function () {
     var _this = $(this);
-    if(_this.next('.js-m-sub-1').hasClass('is-open')) {
-      _this.next('.js-m-sub-1').slideToggle(300, function () {
-        _this.next('.js-m-sub-1').toggleClass('is-open');
+    if(_this.next('.js-sub-1').hasClass('is-open')) {
+      _this.next('.js-sub-1').slideToggle(300, function () {
+        _this.next('.js-sub-1').toggleClass('is-open');
       });
       _this.removeClass('is-active');
       _this.parent().removeClass('is-active');
-    } else if($('.js-m-sub-1.is-open').length > 0) {
-      $('.js-m-sub-1.is-open').slideToggle(300, function () {
+    } else if($('.js-sub-1.is-open').length > 0) {
+      $('.js-sub-1.is-open').slideToggle(300, function () {
         $(this).toggleClass('is-open');
-        _this.next('.js-m-sub-1').slideToggle(300, function () {
-          _this.next('.js-m-sub-1').toggleClass('is-open');
+        _this.next('.js-sub-1').slideToggle(300, function () {
+          _this.next('.js-sub-1').toggleClass('is-open');
         });
       });
-      $('.js-m-root-1').removeClass('is-active');
-      $('.m-menu__item').removeClass('is-active');
+      $('.js-root-1').removeClass('is-active');
+      $('.menu__item').removeClass('is-active');
       _this.addClass('is-active');
       _this.parent().addClass('is-active');
     } else {
-      _this.next('.js-m-sub-1').slideToggle(300, function () {
-        _this.next('.js-m-sub-1').toggleClass('is-open');
+      _this.next('.js-sub-1').slideToggle(300, function () {
+        _this.next('.js-sub-1').toggleClass('is-open');
       });
       _this.addClass('is-active');
       _this.parent().addClass('is-active');
@@ -71,26 +71,26 @@ $(document).ready(function () {
   });
 
   //открываем мобильное выпадающее меню 3 уровня
-  $('.js-m-root-2').click(function () {
+  $('.js-root-2').click(function () {
     var _this = $(this);
-    $('.mobile-menu__top').addClass('mobile-menu__top--open_submenu'); //вешаем класс с нижним отступом для блока с меню
-    $('.m-menu__item').fadeOut(300, function () {
-      $('.js-m-root-1').hide(); //скрываем рутовую сслыку 1 уровня
-      $('.js-m-root-2').hide(); //скрываем рутовую ссылку 2 уровня
-      _this.closest('.m-menu__item').fadeIn(); //показываем блок с меню 3 уровня
-      _this.next('.js-m-sub-2').show(); //показываем меню 3 уровня
+    $('.menu-block__top').addClass('menu-block__top--submenu_open'); //вешаем класс с нижним отступом для блока с меню
+    $('.menu__item').fadeOut(300, function () {
+      $('.js-root-1').hide(); //скрываем рутовую сслыку 1 уровня
+      $('.js-root-2').hide(); //скрываем рутовую ссылку 2 уровня
+      _this.closest('.menu__item').fadeIn(); //показываем блок с меню 3 уровня
+      _this.next('.js-sub-2').show(); //показываем меню 3 уровня
     }); //скрываем пункты меню 1 уровня
     return false;
   });
 
   //закрытие выпадающего меню 3 уровня
-  $('.js-m-root-2-close').click(function () {
-    $('.js-m-sub-2').fadeOut(); //скрываем меню 3 уровня
-    $('.m-menu__item').fadeIn(300, function () {
-      $('.js-m-root-1').fadeIn(); //показываем рутовую сслыку 1 уровня
-      $('.js-m-root-2').fadeIn(); //показываем рутовую ссылку 2 уровня
-      $('.js-m-sub-2').hide(); //скрываем меню 3 уровня
-      $('.mobile-menu__top').removeClass('mobile-menu__top--open_submenu'); //убираем класс с нижним отступом для блока с меню
+  $('.js-root-2-close').click(function () {
+    $('.js-sub-2').fadeOut(); //скрываем меню 3 уровня
+    $('.menu__item').fadeIn(300, function () {
+      $('.js-root-1').fadeIn(); //показываем рутовую сслыку 1 уровня
+      $('.js-root-2').fadeIn(); //показываем рутовую ссылку 2 уровня
+      $('.js-sub-2').hide(); //скрываем меню 3 уровня
+      $('.menu-block__top').removeClass('menu-block__top--submenu_open'); //убираем класс с нижним отступом для блока с меню
     }); //показываем пункты меню 1 уровня
     return false;
   });
